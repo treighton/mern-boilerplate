dc45ecca41a8ee0cb9e8d961b731ee53823c98ec
Your Name <you@example.com>
2023-03-04 20:19:16 -0800

    seeds file added

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/techmatchup",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/puppy-play-date",
   {
 
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndNodify: true,
+    useFindAndNodify: false,
     
   }
 );
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -4,7 +4,31 @@ const typeDefs = gql`
   type User {
     _id: ID!
     username: String!
-    pasword: String
+    pasword: String!
+  }
+
+  type puppy {
+    _id: ID!
+    name: String
+    amount: Int
+    interval:String
+    userId : ID!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User!
+    puppy: ID
+  }
+
+  type Mutation {
+    addUser(username: String!, password: String!): Auth
+    login(username: String!, password: String!): Auth
+    AddPuppy(puppy: PuppyInput!): Puppy
   }
 `;
 module.exports = typeDefs;
