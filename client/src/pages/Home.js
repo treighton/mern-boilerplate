f973703d2d9b363a671fa41e129e277810135d6e
Eugene Dimov <eugenedostoevsky@Eugenes-MBP.MG8702>
2023-03-06 19:09:58 -0800

    .

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Header from '../components/Header.js';
 import Auth from '../utils/auth';
 
 // Need Importations
@@ -8,7 +9,15 @@ import Auth from '../utils/auth';
 // Code incomplete 
 const Home = () => {
     const [loggedIn, setLoggedIn] = useState(Auth.loggedIn());
-    return loggedIn ?  {
-      fetchPolicy: "no-cache"
-    };
+    // return loggedIn ?  {
+    //   fetchPolicy: "no-cache"
+    // }
+    return !loggedIn && ( // remove bang (!) operator
+      <>
+        <Header/>
+        
+      </>
+    )
 }  
+
+export default Home
