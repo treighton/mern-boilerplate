43d777940a6b78907aa0f1a9bbcf49a07227585e
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2023-03-07 18:22:41 -0800

    mutations

--- client/src/components/access/signup.js
+++ client/src/components/access/signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation} from "@apollo/client";
+//import signup mutation when available
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,7 +11,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-
+//insert signup mutation here when available
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -28,14 +28,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+        //plug in mutation when available
+    //   //const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
 
-      const { token, user } = await response.json();
-      console.log(user);
+    //   const { token, user } = await response.json();
+    //   console.log(user);
       Auth.login(token);
     } catch (err) {
       console.error(err);
