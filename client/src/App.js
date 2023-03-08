26d57c37614fa4e0dbf46c2edac86d47a2744232
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2023-03-07 18:32:39 -0800

    added routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import SignupForm from "./components/access/signup";
+import Login from "./components/access/login";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -13,10 +15,13 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Home/>
           <Routes>
             {/* </Routes></Routes> <Route path=“/” element={<Layout />}> */}
             {/* <Route index element={<Home />} />  */}
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<Login />} />
             {/* <Route path=“about” element={<About />} /> */}
             {/* <Route path=“projects” element={<Projects />} /> */}
             {/* <Route path=“contact” element={<Contact />} /> */}
